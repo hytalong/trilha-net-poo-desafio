21c39bb99607f0eecab250be1e4b12739a322ac4
Hytalo P <is.oscar@hotmail.com>
2024-09-26 16:49:36 -0300

    Subindo desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia (numero: "123456", modelo: "Modelo 1", imei:"1111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+System.Console.WriteLine("\n");
+
+System.Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone (numero: "4987", modelo: "Modelo 2", imei:"2222222222", memoria: 128);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
